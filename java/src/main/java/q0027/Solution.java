c3803edc865b9f9d25859d3c2de065d80edfd51c
Lora Hu <lora@artifactlabs.com>
2024-12-23 15:48:21 +0800

    java-q0027 complete

--- java/src/main/java/q0027/Solution.java
+++ java/src/main/java/q0027/Solution.java
@@ -4,8 +4,14 @@ class Solution {
     public int removeElement(int[] nums, int val) {
 
         int index = 0;
-
-
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                int tmp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = tmp;
+                index++;
+            }
+        }
         return index;
     }
 }
